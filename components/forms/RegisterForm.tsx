f4a088d871ebff59bc4b6849355431f92d6235d3
Michał Mistarz <michal.mistarz02@gmail.com>
2024-12-26 18:24:10 +0100

    Added constants

--- components/forms/RegisterForm.tsx
+++ components/forms/RegisterForm.tsx
@@ -121,15 +121,50 @@ const RegisterForm = ({ user }: { user: User }) => {
                 )}
             />
 
+        </div> 
+
+        <div className="flex flex-col gap-6 xl:flex-row">
+            <CustomFormField
+                fieldType={FormFieldType.INPUT}
+                control={form.control}
+                name="address"
+                label="Address"
+                placeholder="16th Street, Cracow"
+            />
+
+            <CustomFormField
+                fieldType={FormFieldType.INPUT}
+                control={form.control}
+                name="occupation"
+                label="Occupation"
+                placeholder="Teacher"
+            />
+
         </div>
 
         <div className="flex flex-col gap-6 xl:flex-row">
+            <CustomFormField
+                fieldType={FormFieldType.INPUT}
+                control={form.control}
+                name="emergencyContactName"
+                label="Emergency contact name"
+                placeholder="michalmistarz10@gmail.com"
+            />
 
+            <CustomFormField
+                fieldType={FormFieldType.PHONE_INPUT}
+                control={form.control}
+                name="emergencyContactNumber"
+                label="Emergency contact number"
+                placeholder="+48 321 321 321"
+            />
         </div>
 
-        <div className="flex flex-col gap-6 xl:flex-row">
-
-        </div>
+        <section className="space-y-6">
+            <div className="mb-9 space-y-1">
+                <h2 className="sub-header"> Medical Information </h2>
+            </div>
+        </section>
 
         <div className="flex flex-col gap-6 xl:flex-row">
 
