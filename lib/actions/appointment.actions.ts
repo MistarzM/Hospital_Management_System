c57f6c1db407c2eb1a69956692ebc6659550f387
Michał Mistarz <michal.mistarz02@gmail.com>
2024-12-27 20:25:14 +0100

    Added appointment actions

---
+++ lib/actions/appointment.actions.ts
@@ -0,0 +1,19 @@
+'use server'
+
+import { ID } from "node-appwrite";
+import { parseStringify } from "../utils";
+import { DATABASE_ID, databases, APPOINTMENT_COLLECTION_ID} from "../appwrite.config";
+
+export const createAppointment = async (appointment: CreateAppointmentParams) => {
+    try {
+        const newAppointment = await databases.createDocument(
+            DATABASE_ID!,
+            APPOINTMENT_COLLECTION_ID!,
+            ID.unique(),
+            appointment
+        )
+        return parseStringify(newAppointment);
+    } catch (error) {
+        console.log(error);
+    }
+}
