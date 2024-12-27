0b1e8b2a519ea8ba0fa80471e77f33de105c1937
Michał Mistarz <michal.mistarz02@gmail.com>
2024-12-27 12:23:54 +0100

    Improved registraiton site's design

--- app/patients/[userId]/register/page.tsx
+++ app/patients/[userId]/register/page.tsx
@@ -9,7 +9,7 @@ const Register = async ({ params: { userId }}: SearchParamProps) => {
 
   return (
     <div className="flex h-screen max-h-screen">
-      <section className="remove-scrollbar container my-auto">
+      <section className="remove-scrollbar container">
         <div className="sub-container max-w-[860px] flex-1 flex-col py-10">
           <Image
             src="/assets/icons/logo-full.svg"
@@ -19,14 +19,9 @@ const Register = async ({ params: { userId }}: SearchParamProps) => {
             className="mb-12 h-10 w-fit"
           />
           <RegisterForm user={user} /> 
-          <div className="text-14-regular mt-20 flex justify-between">
-            <p className="justify-items-end text-dark-600 xl:text-left">
-              © 2025 EHM 
-            </p> 
-            <Link href="/?admin=true" className="text-green-500">
-              Admin  
-            </Link>
-          </div>
+        <p className="copyright py-12">
+            © 2025 EHM 
+        </p> 
         </div>
       </section>
       <Image
