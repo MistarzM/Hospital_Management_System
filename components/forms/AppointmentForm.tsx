0d25b55e3aa62f9d68dc3b484886cf67be15bf71
Michał Mistarz <michal.mistarz02@gmail.com>
2024-12-27 18:51:13 +0100

    Aded structure for scheduling appointment

---
+++ components/forms/AppointmentForm.tsx
@@ -0,0 +1,100 @@
+"use client"
+ 
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { Button } from "@/components/ui/button"
+import { Form } from "@/components/ui/form"
+import CustomFormField from "../CustomFormField"
+import SubmitButton from "../SubmitButton"
+import { useState } from "react"
+import { UserFormValidation } from "@/lib/validation"
+import { useRouter } from "next/navigation"
+import { createUser } from "@/lib/actions/patient.actions"
+import { FormFieldType } from "./PatientForm"
+ 
+ 
+const AppointmentForm = ({
+    userId, patientId, type
+} : {
+    userId: string;
+    patientId: string;
+    type: "create" | "cancel"
+}) => {
+    const router = useRouter();
+    const [isLoading, setIsLoading] = useState(false)
+
+    const form = useForm<z.infer<typeof UserFormValidation>>({
+        resolver: zodResolver(UserFormValidation),
+        defaultValues: {
+            name: "",
+            email: "",
+            phone: "",
+    },
+  })
+ 
+  async function onSubmit({ name, email, phone}: z.infer<typeof UserFormValidation>) {
+    setIsLoading(true)
+
+    try {
+        const userData = { name, email, phone }
+        
+        const user = await createUser(userData);
+
+        if(user) router.push(`/patients/${user.$id}/register`)
+    } catch (error) {
+        console.log(error)
+    }
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
+        <section className="mb-12 space-y-4">
+            <h1 className="header"> New Appointment</h1>
+            <p className="text-dark-700">Request a new appointment</p>
+        </section>
+
+        {type !== "cancel" && (
+            <>
+                <CustomFormField>
+
+                    </CustomFormField> 
+            </>
+        )}
+
+        <CustomFormField
+            fieldType={FormFieldType.INPUT}
+            control={form.control}
+            name="name"
+            label="Full name"
+            placeholder="Michal Mistarz"
+            iconSrc="/assets/icons/user.svg"
+            iconAlt="user"
+        />
+
+        <CustomFormField
+            fieldType={FormFieldType.INPUT}
+            control={form.control}
+            name="email"
+            label="Email"
+            placeholder="michal.mistarz02@gmail.com"
+            iconSrc="/assets/icons/email.svg"
+            iconAlt="email"
+        />
+
+        <CustomFormField
+            fieldType={FormFieldType.PHONE_INPUT}
+            control={form.control}
+            name="phone"
+            label="Phone number"
+            placeholder="+48 123 123 123"
+        />
+
+        <SubmitButton isLoading={ isLoading }>Get Started</SubmitButton>
+      </form>
+    </Form>
+  )
+}
+
+export default AppointmentForm 
